aaf745fb87be22c32410e213095949c7911ce1b5
Jhon Trejos <jhon.trejos@qrvey.com>
2018-07-17 11:31:10 -0500

    fix al crear y esta duplicado

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
 var async = require('async');
 const con = require('../config/database');
+let _ = require('lodash');
 
 const checkExitencia = (email, cedula, done) => {
 
     con.query("SELECT * FROM usuario WHERE email = '" + email + "' OR cedula = " + cedula, (errors, usuario) => { 
         if (errors) return done(errors);
         
-        let campo = (usuario[0].email == email) ? 'email' : 'cedula';
+        let campo = (_.get(usuario[0], 'email', null) == email) ? 'email' : 'cedula';
         return done(null, campo);
         
     });
