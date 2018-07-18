d42ed71d4cbd50eaf75f4bda6d30f4072ad924e7
Jhon Trejos <jhon.trejos@qrvey.com>
2018-07-18 15:41:15 -0500

    Refactoring

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,118 @@
 var async = require('async');
 const con = require('../config/database');
 let _ = require('lodash');
+let moment = require('moment');
+
+let passUtil = require('../utils/passwordUtil');
+
+const crearUsuario = (usuarioParams, done) => {
+    var params = usuarioParams;
+
+    async.parallel([
+        function(callback){
+          passUtil.cryptPassword(params.contrasena, (err, hash) => {
+            if(err)
+              return done(err);
+
+            params.contrasena = hash;
+            var values = Object.keys(params).map(function (key) { return params[key]; });
+            var sql = "INSERT INTO usuario" +
+                      "(nombre," +
+                      "apellido," +
+                      "cedula, " +
+                      "email, " +
+                      "contrasena, " +
+                      "tipo_perfil, " +
+                      "estado, " +
+                      "fecha_creacion, " +
+                      "fecha_modificacion)" + 
+                      "VALUES('" + params.nombre + "', " + 
+                              "'" + params.apellido + "', " +
+                              params.cedula + ", " +
+                              "'" + params.email + "', " +
+                              "'" + params.contrasena + "', " +
+                              params.tipo_perfil + ", " +
+                              params.estado + ", " +
+                              "'" + params.fecha_creacion + "', " +
+                              "'" + params.fecha_modificacion + "')";
+                      
+            con.query(sql, (error, usuario) => {
+                var errorCode = _.get(error, 'code', null);
+                if(errorCode == 'ER_DUP_ENTRY'){
+                  userController.checkExitencia(params.email, params.cedula, (err, duplicado) => {
+                    if (err) return done(err);
+
+                    var msg = "El campo " + duplicado + " ya esta registrado.";
+                    return res.status(300).json({status: 300, 
+                                                message: msg,
+                                                field: duplicado, 
+                                                value: params[duplicado] });
+                  });
+                } else {
+                  callback(error, usuario);
+                }
+              });
+          });
+        }
+      ], (err, data) => {
+            if (err) return done(err);
+
+            return done(err, data);
+      });
+};
+
+const modificarUsuario = (userid, usuarioParams, done) => {
+    var params = usuarioParams;
+    async.waterfall([
+        function(callback){
+          if(_.has(params, 'contrasena')){
+            passUtil.cryptPassword(params.contrasena, (err, hash) => {
+              if(err)
+                return done(err);
+              
+              callback(err, hash);
+            });
+          }else{
+            callback(null, null);
+          }
+        }, function(contra, callback){
+
+          params.contrasena = contra;
+          var sqlUpdateContra = (params.contrasena != null) ? "contrasena = '" + params.contrasena + "', " : "";
+          var sql = "UPDATE usuario SET " +
+                      "nombre = '" + params.nombre + "', " +
+                      "apellido = '" + params.apellido + "', " +
+                      "cedula = " + params.cedula + ", " +
+                      "email = '" + params.email + "', " +
+                      sqlUpdateContra +
+                      "tipo_perfil = " + params.tipo_perfil + ", " +
+                      "estado = " + params.estado + ", " +
+                      "fecha_modificacion = '" + moment().utc().format() + "' " +
+                    "WHERE id = " + userid;
+
+            con.query(sql, (error, usuario) => {
+                var errorCode = _.get(error, 'code', null);
+                if(errorCode == 'ER_DUP_ENTRY'){
+                  userController.checkExitencia(params.email, params.cedula, (err, duplicado) => {
+                    if (err) return done(err);
+
+                    var msg = "El campo " + duplicado + " ya esta registrado.";
+                    return res.status(300).json({status: 300, 
+                                                message: msg,
+                                                field: duplicado, 
+                                                value: params[duplicado] });
+                  });
+                } else {
+                  callback(error, usuario);
+                }
+            });
+        }
+      ], (err, data) => {
+        if (err) return done(err);
+
+        return done(err, data);
+      });
+};
 
 const checkExitencia = (email, cedula, done) => {
 
@@ -14,5 +126,7 @@ const checkExitencia = (email, cedula, done) => {
 };
 
 module.exports = {
-    checkExitencia
+    checkExitencia,
+    crearUsuario,
+    modificarUsuario
 };
--- routes/users.js
+++ routes/users.js
@@ -99,58 +99,13 @@ router.post('/user', ensureToken, function(req, res, next) {
       params.estado = _.get(params, 'estado', 1);
       params.fecha_creacion = moment().utc().format();
       params.fecha_modificacion = moment().utc().format();
-      async.parallel([
-        function(callback){
-          passUtil.cryptPassword(params.contrasena, (err, hash) => {
-            if(err)
-              return next(err);
 
-            params.contrasena = hash;
-            var values = Object.keys(params).map(function (key) { return params[key]; });
-            var sql = "INSERT INTO usuario" +
-                      "(nombre," +
-                      "apellido," +
-                      "cedula, " +
-                      "email, " +
-                      "contrasena, " +
-                      "tipo_perfil, " +
-                      "estado, " +
-                      "fecha_creacion, " +
-                      "fecha_modificacion)" + 
-                      "VALUES('" + params.nombre + "', " + 
-                              "'" + params.apellido + "', " +
-                              params.cedula + ", " +
-                              "'" + params.email + "', " +
-                              "'" + params.contrasena + "', " +
-                              params.tipo_perfil + ", " +
-                              params.estado + ", " +
-                              "'" + params.fecha_creacion + "', " +
-                              "'" + params.fecha_modificacion + "')";
-                      
-            con.query(sql, (error, usuario) => {
-                var errorCode = _.get(error, 'code', null);
-                if(errorCode == 'ER_DUP_ENTRY'){
-                  userController.checkExitencia(params.email, params.cedula, (err, duplicado) => {
-                    if (err) return next(err);
+      userController.crearUsuario(params, (err, data) => {
+        if (err) return next(err);
 
-                    var msg = "El campo " + duplicado + " ya esta registrado.";
-                    return res.status(300).json({status: 300, 
-                                                message: msg,
-                                                field: duplicado, 
-                                                value: params[duplicado] });
-                  });
-                } else {
-                  callback(error, usuario);
-                }
-              });
-          });
-        }
-      ], (err, data) => {
-            if (err) return next(err);
-
-            if (data) return res.json(data);
-            return res.sendStatus(200);
-      });
+        if (data) return res.json(data);
+        return res.sendStatus(200);
+      });      
     }
   });
 });
@@ -165,55 +120,11 @@ router.put('/user/:userid', ensureToken, function(req, res, next) {
       var userid = req.params.userid;
       var params = req.body;
       if(loggedUser[0].id == userid || loggedUser[0].tipo_perfil == 1){
-        async.waterfall([
-          function(callback){
-            if(_.has(params, 'contrasena')){
-              passUtil.cryptPassword(params.contrasena, (err, hash) => {
-                if(err)
-                  return next(err);
-                
-                callback(err, hash);
-              });
-            }else{
-              callback(null, null);
-            }
-          }, function(contra, callback){
+        userController.modificarUsuario(userid, params, (err, data) => {
+          if (err) return next(err);
 
-            params.contrasena = contra;
-            var sqlUpdateContra = (params.contrasena != null) ? "contrasena = '" + params.contrasena + "', " : "";
-            var sql = "UPDATE usuario SET " +
-                        "nombre = '" + params.nombre + "', " +
-                        "apellido = '" + params.apellido + "', " +
-                        "cedula = " + params.cedula + ", " +
-                        "email = '" + params.email + "', " +
-                        sqlUpdateContra +
-                        "tipo_perfil = " + params.tipo_perfil + ", " +
-                        "estado = " + params.estado + ", " +
-                        "fecha_modificacion = '" + moment().utc().format() + "' " +
-                      "WHERE id = " + userid;
-
-              con.query(sql, (error, usuario) => {
-                  var errorCode = _.get(error, 'code', null);
-                  if(errorCode == 'ER_DUP_ENTRY'){
-                    userController.checkExitencia(params.email, params.cedula, (err, duplicado) => {
-                      if (err) return next(err);
-
-                      var msg = "El campo " + duplicado + " ya esta registrado.";
-                      return res.status(300).json({status: 300, 
-                                                  message: msg,
-                                                  field: duplicado, 
-                                                  value: params[duplicado] });
-                    });
-                  } else {
-                    callback(error, usuario);
-                  }
-              });
-          }
-        ], (err, data) => {
-              if (err) return next(err);
-              if (data) return res.json(data);
-              return res.sendStatus(200);
-            // res.render('users');
+          if (data) return res.json(data);
+          return res.sendStatus(200);
         });
       } else {
         var msg = "Solo el Administrador o dueño de la cuenta puede modificar.";
