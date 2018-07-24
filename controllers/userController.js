e51f73e0ac97e968ddbbcb4ee5bfd2e3fa440ba0
Jhon Trejos <jhon.trejos@qrvey.com>
2018-07-24 00:19:45 -0500

    user refactoring

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ var async = require('async');
 const con = require('../config/database');
 let _ = require('lodash');
 let moment = require('moment');
+var mysql = require('mysql');
 
 let passUtil = require('../utils/passwordUtil');
 
@@ -33,27 +34,10 @@ const crearUsuario = (usuarioParams, done) => {
               return done(err);
 
             params.contrasena = hash;
-            var values = Object.keys(params).map(function (key) { return params[key]; });
-            var sql = "INSERT INTO usuario" +
-                      "(nombre," +
-                      "apellido," +
-                      "cedula, " +
-                      "email, " +
-                      "contrasena, " +
-                      "tipo_perfil, " +
-                      "estado, " +
-                      "fecha_creacion, " +
-                      "fecha_modificacion)" + 
-                      "VALUES('" + params.nombre + "', " + 
-                              "'" + params.apellido + "', " +
-                              params.cedula + ", " +
-                              "'" + params.email + "', " +
-                              "'" + params.contrasena + "', " +
-                              params.tipo_perfil + ", " +
-                              params.estado + ", " +
-                              "'" + params.fecha_creacion + "', " +
-                              "'" + params.fecha_modificacion + "')";
-                      
+            var sql = "INSERT INTO usuario SET ?";
+            var inserts = params;
+            sql = mysql.format(sql, inserts);
+
             con.query(sql, (error, usuario) => {
                 var errorCode = _.get(error, 'code', null);
                 if(errorCode == 'ER_DUP_ENTRY'){
@@ -96,34 +80,27 @@ const modificarUsuario = (userid, usuarioParams, done) => {
         }, function(contra, callback){
 
           params.contrasena = contra;
-          var sqlUpdateContra = (params.contrasena != null) ? "contrasena = '" + params.contrasena + "', " : "";
-          var sql = "UPDATE usuario SET " +
-                      "nombre = '" + params.nombre + "', " +
-                      "apellido = '" + params.apellido + "', " +
-                      "cedula = " + params.cedula + ", " +
-                      "email = '" + params.email + "', " +
-                      sqlUpdateContra +
-                      "tipo_perfil = " + params.tipo_perfil + ", " +
-                      "estado = " + params.estado + ", " +
-                      "fecha_modificacion = '" + moment().utc().format() + "' " +
-                    "WHERE id = " + userid;
+          params.contrasena = (params.contrasena != null) ? params.contrasena : " ";
+          var sql = "UPDATE usuario SET ? WHERE id = " + userid;
+          var inserts = params;
+          sql = mysql.format(sql, inserts);
 
-            con.query(sql, (error, usuario) => {
-                var errorCode = _.get(error, 'code', null);
-                if(errorCode == 'ER_DUP_ENTRY'){
-                  userController.checkExitencia(params.email, params.cedula, (err, duplicado) => {
-                    if (err) return done(err);
+          con.query(sql, (error, usuario) => {
+              var errorCode = _.get(error, 'code', null);
+              if(errorCode == 'ER_DUP_ENTRY'){
+                userController.checkExitencia(params.email, params.cedula, (err, duplicado) => {
+                  if (err) return done(err);
 
-                    var msg = "El campo " + duplicado + " ya esta registrado.";
-                    return res.status(300).json({status: 300, 
-                                                message: msg,
-                                                field: duplicado, 
-                                                value: params[duplicado] });
-                  });
-                } else {
-                  callback(error, usuario);
-                }
-            });
+                  var msg = "El campo " + duplicado + " ya esta registrado.";
+                  return res.status(300).json({status: 300, 
+                                              message: msg,
+                                              field: duplicado, 
+                                              value: params[duplicado] });
+                });
+              } else {
+                callback(error, usuario);
+              }
+          });
         }
       ], (err, data) => {
         if (err) return done(err);
