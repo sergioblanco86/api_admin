35ff86d5e44d6f809112e222f0e645b2c5b25e11
Jhon Trejos <jhon.trejos@qrvey.com>
2018-08-27 12:02:13 -0500

    Fix

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 var mysql = require('mysql');
 
 var connection = mysql.createPool({
-    host : 'http://scpt.barranquilla.gov.co:4000/',
+    host : 'scpt.barranquilla.gov.co',
+    port: 4000,
     user : 'root',
     password : '',
     database : 'eventos'
